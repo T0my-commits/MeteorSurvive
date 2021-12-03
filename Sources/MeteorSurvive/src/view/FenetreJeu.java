e2e4daf979f08f0e691618b50ef7b8bcdb8f7f65
Pierre BOURDIAUX <pibourdiau@IUTCLINFA2319.iut.local>
2021-12-03 16:34:47 +0100

    pas grand chose

--- Sources/MeteorSurvive/src/view/FenetreJeu.java
+++ Sources/MeteorSurvive/src/view/FenetreJeu.java
@@ -1,32 +1,41 @@
 package view;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.TitledPane;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import modele.Shoot;
-import java.awt.Dimension;
+
+import java.awt.*;
 
 public class FenetreJeu {
 
+    //@FXML
+    //public ImageView bg;
+
     @FXML
-    public ImageView bg;
+    public Text titre;
+
+    @FXML
+    public BorderPane mainBP;
+
 
     Shoot shoot = new Shoot();
-    Dimension dimension = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
 
-    int height = (int)dimension.getHeight();
-    int width  = (int)dimension.getWidth();
+
+
 
     public void initialize()
     {
-        bg.setPreserveRatio(true);
+        //bg.setPreserveRatio(true);
+        titre.setFont(new Font(50));
 
-        // on adapte la taille de l'image en fonction de l'écran
-        // ne fonctionne pas
-        if (height > width) bg.setFitWidth(width);
-        else bg.setFitHeight(height);
 
     }
 }
